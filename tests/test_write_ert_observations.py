0c24fe0a539c467a467a198fa713fac0249f5319
Daniel Berge Sollien <daniel.sollien@gmail.com>
2024-03-14 15:51:17 +0100

    Add tests for writing ert observations from config file

---
+++ tests/test_write_ert_observations.py
@@ -0,0 +1,47 @@
+"""Test functionality for writing from config file"""
+
+
+def test_reading_config():
+    """Test reading of config file"""
+    pass
+
+
+def test_parse_config_elements():
+    """Test parsing of the individual elements in a config file"""
+
+
+def write_controls_from_config():
+    """Test writing of controls file from config file"""
+    pass
+
+
+def write_summary_observations_from_config():
+    """Test writing of summary observations from config file"""
+
+    pass
+
+
+def write_rft_observations_from_config():
+    """Test writing of rft files from config file"""
+
+    pass
+
+
+def write_general_observations_from_config():
+    """Test writing of general observations from config"""
+    pass
+
+
+def test_write_observations_from_config():
+    """Test writing of all observations from config, might not be a test"""
+    pass
+
+
+def test_write_controls_from_config():
+    """Test writing of controls file from config file"""
+    pass
+
+
+def test_write_observations_from_controls():
+    """Test writing of observations file from control file, might not be a test"""
+    pass
